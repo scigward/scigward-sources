21dfd3b245334e4cd1a79e423e12f977db7e3785
scigward <162128369+scigward@users.noreply.github.com>
2025-04-19 14:40:43 +0300

    Update animerco.js

--- scrapers/animerco/animerco.js
+++ scrapers/animerco/animerco.js
@@ -1,14 +1,19 @@
 function searchResults(html) {
     try {
         const results = [];
-        const itemRegex = /<a href="([^"]+)" class="image lazyactive dbdone" data-src="([^"]+)" title="([^"]+)"/g;
-        let match;
+        const containerRegex = /<div class="page-content">[\s\S]*?<div class="container">[\s\S]*?<div class="row">([\s\S]*?)<\/div>[\s\S]*?<\/div>[\s\S]*?<\/div>/;
+        const containerMatch = html.match(containerRegex);
 
-        while ((match = itemRegex.exec(html)) !== null) {
-            const href = match[1];
-            const image = match[2];
-            const title = match[3];
-            results.push({ title, href, image });
+        if (containerMatch && containerMatch[1]) {
+            const itemRegex = /<a href="([^"]+)" class="image lazyactive dbdone" data-src="([^"]+)" title="([^"]+)"/g;
+            let match;
+
+            while ((match = itemRegex.exec(containerMatch[1])) !== null) {
+                const href = match[1];
+                const image = match[2];
+                const title = match[3];
+                results.push({ title, href, image });
+            }
         }
         return JSON.stringify(results);
     } catch (error) {
