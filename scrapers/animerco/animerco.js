cb1ae949f797282d3208ed08ede6e78ff2973e45
scigward <162128369+scigward@users.noreply.github.com>
2025-04-22 20:34:26 +0300

    Update animerco.js

--- scrapers/animerco/animerco.js
+++ scrapers/animerco/animerco.js
@@ -17,29 +17,27 @@ function searchResults(html) {
 }
 
 function extractDetails(html) {
-    const details = [];
-
-    const descriptionMatch = html.match(/<div class="content">\s*<p>(.*?)<\/p>\s*<\/div>/s);
-    let description = descriptionMatch 
-        ? decodeHTMLEntities(descriptionMatch[1].trim()) 
-        : 'N/A';
-
-    const airdateMatch = html.match(/<li>\s*بداية العرض:\s*<a [^>]*rel="tag"[^>]*>([^<]+)<\/a>\s*<\/li>/);
-    let airdate = airdateMatch ? airdateMatch[1].trim() : '';
-
-    const aliasesMatch = html.match(/<a\s+class="badge yellow-soft"\s+[^>]*?>([^<]+)<\/a>/g);
-    let aliases = aliasesMatch ? aliasesMatch[1].trim() : '';
-
-    if (description && airdate && aliases) {
-        details.push({
-            description: description,
-            aliases: aliases,
-            airdate: airdate
-        });
-    }
-    console.log(details);
-    return details;
-}
+     const details = [];
+ 
+     const descriptionMatch = html.match(/<div class="content">\s*<p>(.*?)<\/p>\s*<\/div>/s);
+     let description = descriptionMatch ? descriptionMatch[1].trim() : '';
+ 
+     const airdateMatch = html.match(/<li>\s*بداية العرض:\s*<a [^>]*rel="tag"[^>]*>([^<]+)<\/a>\s*<\/li>/);
+     let airdate = airdateMatch ? airdateMatch[1].trim() : '';
+ 
+     const aliasesMatch = html.match(/<a[^>]*class="badge yellow-soft"[^>]*>([^<]+)<\/a>/g);
+     let aliases = aliasesMatch ? aliasesMatch[1].trim() : '';
+ 
+     if (description && airdate && aliases) {
+         details.push({
+             description: description,
+             aliases: aliases,
+             airdate: airdate
+         });
+     }
+     console.log(details);
+     return details;
+ }
 
 async function extractEpisodes(url) {
   try {
