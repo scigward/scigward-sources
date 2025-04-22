7f2d295508ca7396ea8d5da9fa657f13d7c2b804
scigward <162128369+scigward@users.noreply.github.com>
2025-04-22 20:25:44 +0300

    Update animerco.js

--- scrapers/animerco/animerco.js
+++ scrapers/animerco/animerco.js
@@ -94,3 +94,21 @@ async function extractStreamUrl(html) {
         return null;
     }
 }
+
+function decodeHTMLEntities(text) {
+    text = text.replace(/&#(\d+);/g, (match, dec) => String.fromCharCode(dec));
+    
+    const entities = {
+        '&quot;': '"',
+        '&amp;': '&',
+        '&apos;': "'",
+        '&lt;': '<',
+        '&gt;': '>'
+    };
+    
+    for (const entity in entities) {
+        text = text.replace(new RegExp(entity, 'g'), entities[entity]);
+    }
+
+    return text;
+}
