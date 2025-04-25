11fa22a79d44f5e42d8df4c4b03bb6e306188598
scigward <162128369+scigward@users.noreply.github.com>
2025-04-25 18:40:03 +0300

    Update okanime.js

--- okanime/okanime.js
+++ okanime/okanime.js
@@ -63,3 +63,21 @@ function extractDetails(html) {
   console.log(details);
   return details;
 }
+
+function decodeHTMLEntities(text) {
+    text = text.replace(/&#(\d+);/g, (match, dec) => String.fromCharCode(dec));
+
+    const entities = {
+        '&quot;': '"',
+        '&amp;': '&',
+        '&apos;': "'",
+        '&lt;': '<',
+        '&gt;': '>'
+    };
+
+    for (const entity in entities) {
+        text = text.replace(new RegExp(entity, 'g'), entities[entity]);
+    }
+
+    return text;
+}
