75c6e596920028783e25cc81fd4e82be4a98aba2
scigward <162128369+scigward@users.noreply.github.com>
2025-05-03 22:58:36 +0300

    Update animeiat.js

--- animeiat/animeiat.js
+++ animeiat/animeiat.js
@@ -2,11 +2,11 @@ async function searchResults(keyword) {
     try {
         const encodedKeyword = encodeURIComponent(keyword);
         const headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
             'Referer': 'https://www.animeiat.xyz/'
         };
 
-        const response = await fetchv2(`https://www.animeiat.xyz/search?q=${encodedKeyword}`, { headers });
+        const response = await fetchv2(`https://www.animeiat.xyz/search?q=${encodedKeyword}`, headers);
         const html = await response.text();
         
         const results = [];
