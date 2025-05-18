f6aeb951a6baab9cae5b4f4437e9e48caba7f63c
scigward <162128369+scigward@users.noreply.github.com>
2025-05-18 19:30:05 +0300

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-//Mocking fetchv2.
+//Mocking fetchv2 if needed.
 async function fetchv2(url, headers) {
   return fetch(url, { headers });
 }
@@ -16,6 +16,29 @@ async function fetchv2(url, headers) {
 
     const streamUrl = await extractStreamUrl("WatchHref");
     console.log('STREAMURL:', streamUrl);
+})();
+
+//or
+(async () => {
+    const results = await searchResults('Cowboy Bebop');
+    console.log('RESULTS:', results);
+
+    const parsedResults = JSON.parse(results);
+    const target = parsedResults[1]; // Index 1 is safe
+
+    const details = await extractDetails(target.href);
+    console.log('DETAILS:', details);
+
+    const eps = await extractEpisodes(target.href);
+    console.log('EPISODES:', eps);
+
+    const parsedEpisodes = JSON.parse(eps);
+    if (parsedEpisodes.length > 0) {
+        const streamUrl = await extractStreamUrl(parsedEpisodes[0].href);
+        console.log('STREAMURL:', streamUrl);
+    } else {
+        console.log('No episodes found.');
+    }
 })();
 
 //Put all this at the top of your main .JS file.
