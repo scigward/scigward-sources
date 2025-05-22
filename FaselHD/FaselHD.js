f86e4401a20a12c55b7b6d89a92606bcbdb7c113
scigward <162128369+scigward@users.noreply.github.com>
2025-05-22 12:15:27 +0300

    Update FaselHD.js

--- FaselHD/FaselHD.js
+++ FaselHD/FaselHD.js
@@ -1,1 +1,44 @@
+async function searchResults(keyword) {
+    try {
+        const encodedKeyword = encodeURIComponent(keyword);
+        const searchUrl = `https://web29.faselhd1watch.one/?s=${encodedKeyword}`;
+        const response = await fetchv2(searchUrl);
+        const responseText = await response.text();
 
+        const results = [];
+
+        const itemRegex = /<div class="col-xl-2 col-lg-2 col-md-3 col-sm-3">\s*<div class="postDiv[^>]*>[\s\S]*?<a href="([^"]+)"[^>]*>[\s\S]*?data-src="([^"]+)"[\s\S]*?alt="([^"]+)"/g;
+        let match;
+
+        while ((match = itemRegex.exec(responseText)) !== null) {
+            const href = match[1].trim();
+            const image = match[2].trim();
+            const title = decodeHTMLEntities(match[3].trim());
+            results.push({ title, href, image });
+        }
+
+        console.log(results);
+        return JSON.stringify(results);
+    } catch (error) {
+        console.log('Fetch error in searchResults:', error);
+        return JSON.stringify([{ title: 'Error', image: '', href: '' }]);
+    }
+}
+
+function decodeHTMLEntities(text) {
+    text = text.replace(/&#(\d+);/g, (match, dec) => String.fromCharCode(dec));
+
+    const entities = {
+        '&quot;': '"',
+        '&amp;': '&',
+        '&apos;': "'",
+        '&lt;': '<',
+        '&gt;': '>'
+    };
+
+    for (const entity in entities) {
+        text = text.replace(new RegExp(entity, 'g'), entities[entity]);
+    }
+
+    return text;
+}
