1205580618a296de75d10093173d5528491b26a1
scigward <162128369+scigward@users.noreply.github.com>
2025-05-25 16:50:35 +0300

    Update test.js

--- test/test.js
+++ test/test.js
@@ -3,22 +3,7 @@ async function fetchv2(url, headers) {
   return fetch(url, { headers });
 }
 
-//test code.
-(async () => {
-    const results = await searchResults('Cowboy Bebop');
-    console.log('RESULTS:', results);
-    
-    const details = await extractDetails("TitlePageHref");
-    console.log('DETAILS:', details);
-
-    const eps = await extractEpisodes("TitlePageHref");
-    console.log('EPISODES:', eps);
-
-    const streamUrl = await extractStreamUrl("WatchHref");
-    console.log('STREAMURL:', streamUrl);
-})();
-
-//or
+// Test code
 (async () => {
     const results = await searchResults('Cowboy Bebop');
     console.log('RESULTS:', results);
@@ -41,4 +26,20 @@ async function fetchv2(url, headers) {
     }
 })();
 
+// Or
+(async () => {
+    const results = await searchResults('Cowboy Bebop');
+    console.log('RESULTS:', results);
+    
+    const details = await extractDetails("TitlePageHref");
+    console.log('DETAILS:', details);
+
+    const eps = await extractEpisodes("TitlePageHref");
+    console.log('EPISODES:', eps);
+
+    const streamUrl = await extractStreamUrl("WatchHref");
+    console.log('STREAMURL:', streamUrl);
+})();
+
+//or
 //Put all this at the top of your main .JS file.
