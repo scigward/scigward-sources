c7996b95f2d71ad432241229a67679ca82399d56
scigward <162128369+scigward@users.noreply.github.com>
2025-07-17 12:22:26 +0300

    Added a deobfuscator in utilities

--- utilities/utilities.js
+++ utilities/utilities.js
@@ -16,3 +16,126 @@ function decodeHTMLEntities(text) {
 
     return text;
 }
+
+/**
+ * Extracts and deobfuscates an obfuscated script from the given HTML content.
+ * @param {string} html - The HTML content containing the obfuscated script.
+ * @returns {string|null} The deobfuscated script, or null if no obfuscated script is found.
+ */
+function deobfuscate(html) {
+    const obfuscatedScript = html.match(/<script[^>]*>\s*(eval\(function\(p,a,c,k,e,d.*?\)[\s\S]*?)<\/script>/);
+    if(!obfuscatedScript) return null;
+
+    const unpackedScript = unpack(obfuscatedScript[1]);
+    return unpackedScript;
+}
+
+/*
+ * DEOBFUSCATOR CODE
+ */
+class Unbaser {
+    constructor(base) {
+        this.ALPHABET = {
+            62: "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+            95: "' !\"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~'",
+        };
+        this.dictionary = {};
+        this.base = base;
+        if (36 < base && base < 62) {
+            this.ALPHABET[base] = this.ALPHABET[base] ||
+                this.ALPHABET[62].substr(0, base);
+        }
+        if (2 <= base && base <= 36) {
+            this.unbase = (value) => parseInt(value, base);
+        }
+        else {
+            try {
+                [...this.ALPHABET[base]].forEach((cipher, index) => {
+                    this.dictionary[cipher] = index;
+                });
+            }
+            catch (er) {
+                throw new Error('Unsupported base encoding.');
+            }
+            this.unbase = this._dictunbaser;
+        }
+    }
+    _dictunbaser(value) {
+        let ret = 0;
+        [...value].reverse().forEach((cipher, index) => {
+            ret = ret + ((Math.pow(this.base, index)) * this.dictionary[cipher]);
+        });
+        return ret;
+    }
+}
+
+/**
+ * Checks if a given source code (JS File) is obfuscated with the p.a.c.k.e.r. algorithm.
+ * @param {string} source - The source code (JS File) to check.
+ * @returns {boolean} true if the source code is obfuscated with p.a.c.k.e.r., false otherwise.
+ */
+function detect(source) {
+    return source.replace(" ", "").startsWith("eval(function(p,a,c,k,e,");
+}
+
+/**
+ * Unpacks a given source code (JS File) that is obfuscated with the p.a.c.k.e.r. algorithm.
+ * @param {string} source - The source code (JS File) to unpack.
+ * @returns {string} The unpacked source code.
+ * @throws {Error} If the source code is not obfuscated with p.a.c.k.e.r. or if the data is corrupted.
+ */
+function unpack(source) {
+    let { payload, symtab, radix, count } = _filterargs(source);
+    if (count != symtab.length) {
+        throw new Error('Malformed p.a.c.k.e.r. symtab.');
+    }
+    let unbase;
+    try {
+        unbase = new Unbaser(radix);
+    }
+    catch (e) {
+        throw new Error('Unknown p.a.c.k.e.r. encoding.');
+    }
+    function lookup(match) {
+        const word = match;
+        let word2;
+        if (radix == 1) {
+            word2 = symtab[parseInt(word)];
+        }
+        else {
+            word2 = symtab[unbase.unbase(word)];
+        }
+        return word2 || word;
+    }
+    source = payload.replace(/\b\w+\b/g, lookup);
+    return _replacestrings(source);
+    function _filterargs(source) {
+        const juicers = [
+            /}\('(.*)', *(\d+|\[\]), *(\d+), *'(.*)'\.split\('\|'\), *(\d+), *(.*)\)\)/,
+            /}\('(.*)', *(\d+|\[\]), *(\d+), *'(.*)'\.split\('\|'\)/,
+        ];
+        for (const juicer of juicers) {
+            const args = juicer.exec(source);
+            if (args) {
+                let a = args;
+                if (a[2] == "[]") {
+                }
+                try {
+                    return {
+                        payload: a[1],
+                        symtab: a[4].split("|"),
+                        radix: parseInt(a[2]),
+                        count: parseInt(a[3]),
+                    };
+                }
+                catch (ValueError) {
+                    throw new Error('Corrupted p.a.c.k.e.r. data.');
+                }
+            }
+        }
+        throw new Error('Could not make sense of p.a.c.k.e.r data (unexpected code structure)');
+    }
+    function _replacestrings(source) {
+        return source;
+    }
+}
