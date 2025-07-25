af4bc4241bddd6d09dbae7e97b6d0644f26509c1
Admin <malekalmutairi305@gmail.com>
2025-07-25 04:04:25 +0300

    This sucks

--- YW5pbWVyY28K/YW5pbWVyY28K.js
+++ YW5pbWVyY28K/YW5pbWVyY28K.js
@@ -174,7 +174,9 @@ async function extractStreamUrl(url) {
                 const headers = {
                     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36',
                     'Origin': DECODE_SI(),
-                    'x-requested-with': 'XMLHttpRequest',
+                    'X-Requested-With': 'XMLHttpRequest',
+                    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                    'path': '/wp-admin/admin-ajax.php',
                     'Referer': url,
                 };
 
